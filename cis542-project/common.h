d1ae026992343586cd8cb15e5ffc48dfa30fadcc
wenruimeng <wenruimeng@gmail.com>
2014-04-21 16:10:45 -0400

    add the common data structure file

---
+++ cis542-project/common.h
@@ -0,0 +1,37 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+class Direction {
+public:
+  enum direction{up, down, right, left}; //the direction is for x-y plane. up means direction in the y-axis and y-->biggest 
+    Direction(const Direction& d);
+  double getAngularVelocity(direction target); 
+
+    // rotated 1/4 turn to the right
+    Direction right();
+    // rotated 1/4 turn to the left
+    Direction left();
+    // this direction rotated 1/2 turn
+    Direction operator -();
+};
+
+
+class Position {
+    int x,y;
+public:
+    Position operator +(Direction d);
+    Position operator -(Direction d);
+    Position& operator +=(Direction d);
+    Position& operator -=(Direction d);
+    Position& operator =(Position p);
+};
+
+
+
+
+#endif

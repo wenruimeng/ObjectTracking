4cd8d3d2e0b97fd18c41e42ae37a42a6c501fe66
wenruimeng <wenruimeng@gmail.com>
2014-04-22 06:57:45 -0400

    remote follower. It is not necessary for the velocity publish, since path transfer will do that job.

--- cis542-project/pathTransfer.cpp
+++ cis542-project/pathTransfer.cpp
@@ -12,6 +12,48 @@ Direction currentDirection;
 Point currentPosition;
 
 double PathTransfer::getAngle(Point current, Point next){
+  assert(!(current.x == next.x  && current.y == next.y));
+
+  if(current.x > next.x) {
+    switch(currentDirection) {
+    case "right": return 1/2 * pi;
+    case "left": return 0;
+    case "up": return -1/4 * pi;
+    case "down": return 1/4 * pi;
+    }
+  }
+
+  if(current.x < next.x) {
+    switch(currentDirection) {
+    case "right": return 0 * pi;
+    case "left": return 1/2 * pi;
+    case "up": return 1/4 * pi;
+    case "down": return -1/4 * pi;
+    }
+  }
+
+  if(current.y > next.y) {
+    switch(currentDirection) {
+    case "right": return 1/4 * pi;
+    case "left": return -1/4 * pi;
+    case "up": return 1/2 * pi;
+    case "down": return 0 * pi;
+    }
+  }
+
+
+  if(current.y < next.y) {
+    switch(currentDirection) {
+    case "right": return -1/4 * pi;
+    case "left": return 1/4  * pi;
+    case "up": return 0;
+    case "down": return 1/2 * pi;
+    }
+  }
+
+
+  exit(0);
+
 
 }
 
@@ -41,6 +83,11 @@ int main(int argc, char **argv) {
       else{
 	cmd.angular.z = ang * 10;
 	velocityPub.publish(cmd);
+	sleep(0.1);
+	cmd.angular.z = 0;
+	cmd.linear.x = speed;
+	velocityPub.publish(cmd);
+	currentPosition = currentPath[i];
 	sleep(0.1);
       }
     r.sleep();
